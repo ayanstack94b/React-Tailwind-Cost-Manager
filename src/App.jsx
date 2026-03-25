33fe478dc7773107fb2e32d3fbe929cb82ed884f
unknown <ayondevs.94b@gmail.com>
2026-03-25 14:59:06 +0530

    added headr with custom dynamic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-6xl'>react app</h1>
+      <header>
+        <Navbar></Navbar>
+      </header>
     </>
   )
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Link from './Link';
+
+const navData = [
+    {
+        id: 1,
+        name: "Home",
+        path: "/"
+    },
+    {
+        id: 2,
+        name: "About",
+        path: "/about"
+    },
+    {
+        id: 3,
+        name: "Projects",
+        path: "/projects"
+    },
+    {
+        id: 4,
+        name: "Blog",
+        path: "/blog"
+    },
+    {
+        id: 5,
+        name: "Contact",
+        path: "/contact"
+    }
+];
+
+const Navbar = () => {
+    return (
+        <nav className='mt-5'>
+            <ul className='flex ml-10'>
+                {
+                    navData.map((route) => <Link
+                        key={route.id}
+                        route={route}
+                    ></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
